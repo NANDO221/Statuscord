da8e38ad719c2dbdf74a227d5383ef76631601f5
NANDO221 <nandodavis2011@gmail.com>
2025-01-25 19:38:25 -0500

    Update streaming.js

--- statuses/streaming.js
+++ statuses/streaming.js
@@ -2,10 +2,10 @@ const
   rpcGenerator = require("discordrpcgenerator"),
 
   // Add your client id in the index.js file
-  IMAGE_NAME = "The name of the image",
-  LARGE_TEXT = "Large text on the status",
-  SMALL_TEXT = "Small text on the status",
-  LINK = "https://twitch.tv/SealedSaucer";
+  IMAGE_NAME = "The name of the image", osamason
+  LARGE_TEXT = "Large text on the status",Watch
+  SMALL_TEXT = "Small text on the status",watch
+  LINK = https://youtu.be/-KKbdErJkiY?si=JsP_m-UadrkyXUge;
  
 module.exports = (client, CLIENT_ID) => rpcGenerator.getRpcImage(CLIENT_ID, IMAGE_NAME)
   .then(image => client.user.setPresence(
